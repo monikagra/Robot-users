8c5881a47d3af5377bf308e2eb6e31acafd10738
Monika <monika.grazlewska@gmail.com>
2020-04-27 15:10:43 +0200

    Updates

---
+++ precache-manifest.aa5d4ef08229319455067fc5679594fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5917a125afcaa9f25b85ae32d2f79e",
+    "url": "/monikagra/Robot-users/index.html"
+  },
+  {
+    "revision": "ac86c41730508cb1152a",
+    "url": "/monikagra/Robot-users/static/css/main.c977fcb1.chunk.css"
+  },
+  {
+    "revision": "98081d326b58d5155cd5",
+    "url": "/monikagra/Robot-users/static/js/2.5764ffd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monikagra/Robot-users/static/js/2.5764ffd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac86c41730508cb1152a",
+    "url": "/monikagra/Robot-users/static/js/main.0c70b15a.chunk.js"
+  },
+  {
+    "revision": "943065cbc82b5ab14712",
+    "url": "/monikagra/Robot-users/static/js/runtime-main.0caad8cc.js"
+  }
+]);
